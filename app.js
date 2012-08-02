40913e70e5f911619d774d5ff0d571c92085e0a4
blitz <ellliotlai@gmail.com>
2012-08-02 18:27:38 +0800

    url rewrite fix

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ var connect = require('connect'),
     path    = require('path');
 
 var urlRewrite = function (req, res, next) {
-
     if (req.headers.referer && path.extname(req.url) !== '')
         req.url = '/' + path.relative('/' + path.dirname(path.relative('http://' + req.headers.host, req.headers.referer)), req.url);          
     else
-        req.url = '';            
+        req.url = '';         
     next();        
 };
 
@@ -18,15 +17,24 @@ var xhr = function (req, res, next) {
         next();
 }
 
+var api = function (req, res, next) {
+    var apiReq = /^\/api\//.test(req.url)
+
+    if (apiReq) 
+        request('http://itswindtw.info:9001' + req.url).pipe(res);
+    else
+        next()
+}
+
 var server0 = connect()
     .use(urlRewrite)
-    .use(xhr)
+    .use(api)
     .use(connect.static(__dirname + '/public'))
     .listen(4000);
     
     
 var server1 = connect()
     .use(urlRewrite)
-    .use(xhr)
+    .use(api)
     .use(connect.static(__dirname + '/../cedar'))
     .listen(3000);
