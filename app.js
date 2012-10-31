6148ced9272a4b48fcdf2041fce63fa137c72c10
blitz <banacorn@gmail.com>
2012-10-31 12:50:04 +0800

    static resolve

--- app.js
+++ app.js
@@ -2,13 +2,29 @@ var connect = require('connect'),
     request = require('request'),
     path    = require('path');
 
+
+var staticAssets = [
+    'images',
+    'scripts',
+    'stylesheets',
+    'fonts'
+]
+
+
+
 var urlRewrite = function (req, res, next) {
-    if (req.headers.referer) {
+    if (req.headers.referer || /\/scripts\//.test(req.url)) {
         req.url = '/' + path.relative('/' + path.dirname(path.relative('http://' + req.headers.host, req.headers.referer)), req.url);          
         req.url = req.url.replace(/\/\.\./g, '');
     } else {
         req.url = '';         
     }
+
+    // is not static asset request
+    notStatic = staticAssets.indexOf(req.url.split(path.sep)[1]) == -1;
+    if (notStatic)
+        req.url = '/'
+
     next();        
 };
 
@@ -21,7 +37,9 @@ var xhr = function (req, res, next) {
 }
 
 var api = function (req, res, next) {
-    var apiReq = /\/api\//.test(req.url)
+
+    var apiReq = /\/api\//.test(req.url);
+    
     if (apiReq) {
         request('http://itswindtw.info:9001' + req.url).pipe(res);
     } else {
